5fb97e89422e51425fb333f341a7b6d95851faa5
SliesarenkoDima <slesarenkodima1996@gmail.com>
2026-09-04 00:12:19 +0300

    added Keycloak

--- Aspire/Evently.AppHost/AppHost.cs
+++ Aspire/Evently.AppHost/AppHost.cs
@@ -16,11 +16,23 @@ IResourceBuilder<RedisResource> cache = builder
     .AddRedis("cache", port: 6379)
     .WithLifetime(ContainerLifetime.Persistent);
 
+IResourceBuilder<KeycloakResource> keycloak = builder
+    .AddKeycloak(
+        "keycloak",
+        8080,
+        builder.CreateResourceBuilder(new ParameterResource("keycloak-user", _ => "admin")),
+        builder.CreateResourceBuilder(new ParameterResource("keycloak-password", _ => "admin"))
+    )
+    .WithDataVolume("keycloak-data-volume")
+    .WithLifetime(ContainerLifetime.Persistent);
+
 builder
     .AddProject<Projects.Evently_Api>("evently-api")
     .WithEnvironment("ConnectionStrings__Database", db)
     .WithReference(cache)
+    .WithReference(keycloak)
     .WaitFor(db)
-    .WaitFor(cache);
+    .WaitFor(cache)
+    .WaitFor(keycloak);
 
 await builder.Build().RunAsync();
